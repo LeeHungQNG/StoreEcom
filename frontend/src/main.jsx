a7008b04f09608b3c8588ec1fbce16efe34917ae
LeeHungQNG <builequochung3004@gmail.com>
2024-11-22 15:13:11 +0700

    User management and auth BE and FE

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,41 @@
-import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App.jsx';
+import { Provider } from 'react-redux';
+import store from './redux/store';
+import { Route, RouterProvider, createRoutesFromElements } from 'react-router';
+import { createBrowserRouter } from 'react-router-dom';
+
+import PrivateRoute from './components/PrivateRoute.jsx';
+// Auth
+import Login from './pages/Auth/Login.jsx';
+import Register from './pages/Auth/Register.jsx';
+
+import Profile from './pages/User/Profile.jsx';
+import AdminRoute from './pages/Admin/AdminRoute.jsx';
+import UserList from './pages/Admin/UserList.jsx';
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<App />}>
+      <Route path="/login" element={<Login />} />
+      <Route path="/register" element={<Register />} />
+
+      {/* Registered users */}
+      <Route path="" element={<PrivateRoute />}>
+        <Route path="/profile" element={<Profile />} />
+      </Route>
+
+      {/* Admin Routes */}
+      <Route path="/admin" element={<AdminRoute />}>
+        <Route path="userlist" element={<UserList />} />
+      </Route>
+    </Route>
+  )
+);
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>
+  <Provider store={store}>
+    <RouterProvider router={router} />
+  </Provider>
 );
