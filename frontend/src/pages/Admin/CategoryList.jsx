0f9eac9bb4b537aa1a5b358bf4351bd4547ef0a9
LeeHungQNG <builequochung3004@gmail.com>
2024-11-26 17:19:11 +0700

    CRUD Product admin

--- frontend/src/pages/Admin/CategoryList.jsx
+++ frontend/src/pages/Admin/CategoryList.jsx
@@ -1,8 +1,9 @@
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 import { useCreateCategoryMutation, useDeleteCategoryMutation, useFetchCategoriesQuery, useUpdateCategoryMutation } from '../../redux/api/categoryApiSlice';
 import { toast } from 'react-toastify';
 import CategoryForm from '../../components/CategoryForm';
 import Modal from '../../components/Modal';
+import AdminMenu from './AdminMenu';
 
 const CategoryList = () => {
   const { data: categories, refetch } = useFetchCategoriesQuery();
@@ -88,7 +89,7 @@ const CategoryList = () => {
 
   return (
     <div className="ml-[10rem] flex flex-col md:flex-row">
-      {/* <AdminMenu /> */}
+      <AdminMenu />
       <div className="md:w-3/4 p-3">
         <div className="h-12">Manage Categories</div>
         <CategoryForm value={name} setValue={setName} handleSubmit={handleCreateCategory} />
